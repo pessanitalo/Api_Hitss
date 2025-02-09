990435933e7f82c585f7d064285c60f7f8870092
italo pessan <92962625+pessanitalo@users.noreply.github.com>
2025-02-09 10:52:31 -0300

    interface do calculo dos serviços

---
+++ Hitss/Api_Hitss/Interface/IPaymentScheduleCalcService.cs
@@ -0,0 +1,12 @@
+﻿using Api_Hitss.Model;
+
+namespace Api_Hitss.Interface
+{
+    public interface IPaymentScheduleCalcService
+    {
+        Decimal Juros(Proposta proposta);
+        Decimal Amortizacao();
+        Decimal SaldoAtual();
+        Decimal ParcelaMensalFixa(Proposta proposta);
+    }
+}
